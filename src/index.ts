450393d1c9998dc71b81b7c667d2b4cfde3e393c
Vidhu <vidhu23110103@snuchennai.edu.in>
2025-08-24 19:04:07 +0530

    Migrating Flask to Node

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import app from "./server";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.EXPRESS_PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
